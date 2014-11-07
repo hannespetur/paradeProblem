a71b5c5a6ecc51102c8e4102a2903e68cf8ba09e
Hannes Pétur Eggertsson <hannespetur@gmail.com>
2014-11-07 18:54:01 +0000

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# The Parade Problem
+## Overview
+[Parade](http://boardgamegeek.com/boardgame/56692/parade) is a card game published 2007. The cards are of six different colours/sorts and have a rank of 0-10, so in total 66 cards.
+
+This algorithm tries to find the longest possible row of cards that do not break the games' two rules (if you break those rules, you'll need to take someone out of the parade):
+ 1. If there exists a card of rank i and at position j there cannot be a card at positions >= j+i with rank >= i.
+ 2. If there exists a card of rank i, at positions j and some type there cannot be a card of the same type at positions >= j+i of any rank.
+ 
+ So far the longest possible row I've found using this algorithm is 24.
--- paradeProblem.py
+++ paradeProblem.py
@@ -1,92 +1,71 @@
-## The Parade Problem
-#  Create the longest possible row of cards that do not break these rules:
-#    1. If there exists a card of rank i and at position j there cannot be a card at positions >= j+i with rank >= i.
-#    2. If there exists a card of rank i, at positions j and some type there cannot be a card of the same type at positions >= j+i of any rank.
-import itertools
-import collections
 import copy
 import os
 
-os.chdir('c:/')
-f = open('paradaProblemOutput.txt','w')
-	
-def validRow(row):
-	# Input: row (list)
-	# Output: True/false, if the row valid.
-	checkThis = row[-1]
-	if row.count(checkThis)>6:
-		return False
-	for pos in xrange(len(row)-11):
-		if checkThis > row[pos]:
-			return False
+def nextCards(row,uniqueSorts,lengd):
+	# Input: row (list), uniqueSorts (int) and lengd (int)
+	# Output: list of new valid rows (list of lists)
+	resultRow = []
+	resultUniqueSorts = []
+	lengd += 1
+	for sort in xrange(0,min(100*(uniqueSorts+1),600),100):
+		for card in reversed(xrange(11)):
+			temp = copy.deepcopy(row)
+			temp.append(card+sort)
+			if card+sort not in set(row) and validRow(temp,lengd):
+				resultRow.append(temp)
+				if sort > ((uniqueSorts-1)*100):
+					resultUniqueSorts.append(uniqueSorts+1)
+				else:
+					resultUniqueSorts.append(uniqueSorts)
+	return resultRow, resultUniqueSorts, lengd
 
-	for pos in xrange(len(row)):
-		if pos==0:
-			sorts = [0]
-		else:
-			sort = checkLastRow(row[:pos+1],sorts)
-			if sort != 6:
-				sorts += [sort]
-				# print "Sorts and row are:",sorts
-				# print row
-			else:
+def validRow(row,lengd):
+	checkRow = row[-1]
+	checkSort, checkRank = splitRow(checkRow)
+	for i in xrange(lengd-1):
+		compareRow = row[i]
+		compareSort, compareRank = splitRow(compareRow)
+		if compareRank+i < lengd-1:
+			if compareSort == checkSort or compareRank <= checkRank:
 				return False
-	# print "Sorts and row are:",sorts
-	# print row
 	return True
 
-def checkLastRow(row,sorts):
-	def checkRowForSort(row,checkSort):
-		checkRow = row[-1]
-		for pos1 in xrange(len(row)-1):
-			for pos2 in xrange(len(row)-1):
-				if checkSort == sorts[pos1] and pos1+row[pos1]<=pos2:
-					return False
-			if checkSort == sorts[pos1] and checkRow == row[pos1]:
-				return False
-		return True
-	for sort in xrange(6):
-		if checkRowForSort(row,sort):
-			return sort
-	return 6
-
-def nextCards(row):
-	# Input: row (list)
-	# Output: list of new valid rows (list of lists)
-	result = []
-	for card in reversed(xrange(11)):
-		temp = copy.deepcopy(row)
-		temp.append(card)
-		result.append(temp)
-	newRows = [rows for rows in result if validRow(rows)]
-	return newRows
-
-def problemSolved(row,sorts):
-	if row != False and len(row) >= condition and validRow(row):
-		return True
+def splitRow(row):
+	sort = row/100
+	rank = row-100*sort
+	return sort, rank
+	
+def solveRow(row,uniqueSorts,lengd):
+	if problemSolved(row,uniqueSorts,lengd):
+		return row,uniqueSorts,lengd
 	else:
-		return False
+		newRows, resultUniqueSorts, newlengd = nextCards(row,uniqueSorts,lengd)
+		return solveRowList(newRows,resultUniqueSorts,newlengd)
+	
+def problemSolved(row,uniqueSorts,lengd):
+	global condition
+	if row != False and len(row) >= condition and validRow(row,lengd):
+		condition = len(row)
+		f.write(str(lengd)+' '+str(row)+'\n')
+	return False
 
-def solveRow(row,sorts):
-	if problemSolved(row):
-		return row
-	else:
-		return solveRowList(nextCards(row))
-
-def solveRowList(rowlist,sortslist):
+def solveRowList(rowlist,uniqueSortslist,lengd):
 	if rowlist == []:
-		return False
+		return False, 0, 0
 	else:
-		check = solveRow(rowlist[0])
-		if problemSolved(check):
+		checkRow, checkSorts, checkLengd = solveRow(rowlist[0],uniqueSortslist[0],lengd)
+		if problemSolved(checkRow,checkSorts,checkLengd):
 			f.write("Problem solved!\n")
-			f.write(','.join(str(e) for e in list(check))+'\n')
-			return check
+			f.write(','.join(str(e) for e in list(checkRow))+'\n')
+			return checkRow, checkSorts, checkLengd
 		else:
-			return solveRowList(rowlist[1:])
-			
-row = [9]
-sorts = [0]
-condition = 16
-print solveRow(row)
+			return solveRowList(rowlist[1:], uniqueSortslist[1:], lengd)
+
+os.chdir('c:/')
+f = open('paradaProblemOutput.txt','w')
+row = []
+uniqueSorts = len(list(set([el/100 for el in row])))
+condition = 20
+lengd = len(row)
+print solveRow(row,uniqueSorts,lengd)
 f.close()
